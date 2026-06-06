da9d605c7a4f6284e2ae24b01dd7069fb9906663
Ravinder Sirohi <ravinder@hovergen.com>
2026-06-06 23:35:19 +0100

    IAM role updated

--- infra/iam._ec2.tf
+++ infra/iam._ec2.tf
@@ -22,3 +22,26 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role       = aws_iam_role.ec2_role.name
   depends_on = [aws_iam_role.ec2_role]
 }
+
+resource "aws_iam_role" "eb_service_role" {
+  name = "${var.app}-beanstalk-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "elasticbeanstalk.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eb_service_role_service" {
+  role       = aws_iam_role.eb_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
+}
+
+resource "aws_iam_role_policy_attachment" "eb_service_role_health" {
+  role       = aws_iam_role.eb_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
+}
